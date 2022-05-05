ea4553538d483d3e6d65c9cc99f0370841649902
Hajara <hajaraiyal@gmail.com>
2022-05-05 13:48:39 +0100

    rendering week cards

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,11 +1,14 @@
-import './Card.css';
+import "./Card.css";
 
-const Card = ({cityName, temperature, icon}) => {
-  return <div className="card">
-<h1>{cityName}</h1>
-<p>{temperature}</p>
-<img src={icon} alt="weather icon"/>
-  </div>;
+const Card = ({ cityName, temperature, day }) => {
+	return (
+		<div className="card">
+			<h1>{cityName}</h1>
+			<p>{temperature}</p>
+			<p>{day}</p>
+			{/* <img src={icon} alt="weather icon" /> */}
+		</div>
+	);
 };
 
 export default Card;
