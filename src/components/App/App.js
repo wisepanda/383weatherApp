d20fcf5a757c267235232f2aa7eda251127e3eb8
Hajara <hajaraiyal@gmail.com>
2022-05-05 12:10:43 +0100

    searchbar user input

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,40 +1,41 @@
-import './App.css';
-import { useEffect, useState } from 'react';
+import "./App.css";
+import { useEffect, useState } from "react";
 //Components
-import Card from '../Card';
-import SearchBar from '../SearchBar';
-import WeekList from '../WeekList';
+import Card from "../Card";
+import SearchBar from "../SearchBar";
+import WeekList from "../WeekList";
 
 function App() {
-  const [current, setCurrent] = useState('');
-  const [weekly, setWeekly] = useState('');
+	const [current, setCurrent] = useState("");
+	const [weekly, setWeekly] = useState("");
+	const [location, setLocation] = useState("");
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const cityCoordinates = await fetch(
-        `https://api.openweathermap.org/data/2.5/weather?q=Lisbon&units=metric&appid=f9c0898eeee0f76ed20293b748929eca`
-      );
-      const coordinatesData = await cityCoordinates.json();
-      const lon = coordinatesData.coord.lon;
-      const lat = coordinatesData.coord.lat;
+	useEffect(() => {
+		const fetchData = async () => {
+			const cityCoordinates = await fetch(
+				`https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=f9c0898eeee0f76ed20293b748929eca`
+			);
+			const coordinatesData = await cityCoordinates.json();
+			const lon = coordinatesData.coord.lon;
+			const lat = coordinatesData.coord.lat;
 
-      const res = await fetch(
-        `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&exclude=minutely&appid=f9c0898eeee0f76ed20293b748929eca`
-      );
-      const Data = await res.json();
-      setCurrent(Data.current);
-      setWeekly(Data.daily);
-    };
-    fetchData();
-  }, []);
+			const res = await fetch(
+				`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&exclude=minutely&appid=f9c0898eeee0f76ed20293b748929eca`
+			);
+			const Data = await res.json();
+			setCurrent(Data.current);
+			setWeekly(Data.daily);
+		};
+		fetchData();
+	}, []);
 
-  return (
-    <div className="App">
-      <SearchBar />
-      <Card />
-      <WeekList />
-    </div>
-  );
+	return (
+		<div className="App">
+			<SearchBar setLocation={setLocation} />
+			{/* <Card />
+			<WeekList /> */}
+		</div>
+	);
 }
 
 export default App;
--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,10 +1,18 @@
 import "./searchbar.css";
+import { useState } from "react";
 
-const SearchBar = () => {
+const SearchBar = ({ setLocation, handleClick }) => {
+	const [userInput, setUserInput] = useState("");
+
+	function handleInput(e) {
+		setUserInput(e.target.value);
+		console.log(e.target.value);
+	}
+
 	return (
 		<div className="searchbar">
-			<input type="text" placeholder="Enter city name" />
-			<button>Search</button>
+			<input type="text" placeholder="Enter city name" onChange={handleInput} />
+			<button onClick={(e) => handleClick(userInput)}>Search</button>
 		</div>
 	);
 };
